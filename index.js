57361f96a196f7fba2c7c902976aeac8149cc68d
John Ivar Haugland <jiha@equinor.com>
2019-06-04 11:02:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get("/", function(req, res) {
 
 io.on("connection", function(socket) {
   console.log("a user connected");
+  console.log(socket.conn.transport.name);
 
   socket.on("chat message", function(msg) {
     console.log("message: ", msg);
