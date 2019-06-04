a8e6cbcbd5860903f7ad24db4f483b59f24adff4
John Ivar Haugland <jiha@equinor.com>
2019-06-04 10:47:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-io.set('transports', ['websocket']);
+var server = require("http").Server(app);
+var io = require("socket.io")(server, {
+  transports: ['websocket'],
+  perMessageDeflate: true,
+  httpCompression: true,
+  wsEngine: 'ws'
+});
+//io.set('transports', ['websocket']);
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -20,6 +25,6 @@ io.on("connection", function(socket) {
   });
 });
 
-http.listen(3000, function() {
+server.listen(3000, function() {
   console.log("listening on *:3000");
 });
